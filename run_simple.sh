004d8163841cd2f091cba898b70a412a20a6995c
HelloKitty9292 <hsuanjul@andrew.cmu.edu>
2026-04-20 11:25:59 -0400

    run simple fix

--- run_simple.sh
+++ run_simple.sh
@@ -1,7 +1,7 @@
 export EFFCC_DIR="/home/argus/effcc/"
 mkdir ../../build
 cd ../../build
-cmake -G Ninja . -DEFF_STDIO_PORT=3
+cmake -G Ninja .. -DEFF_STDIO_PORT=3
 ninja
 sudo /home/argus/effcc/bin/eff-flash apps/Avionics-Argus3-SDR/scalar/Avionics-Argus3-SDR.hex sram
 
