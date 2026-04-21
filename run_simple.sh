76cca6f4c1f641bf25b8eb22a90a9966c5b92d96
HelloKitty9292 <hsuanjul@andrew.cmu.edu>
2026-04-21 15:38:18 -0400

    update run simple

--- run_simple.sh
+++ run_simple.sh
@@ -10,6 +10,4 @@ cmake -G Ninja .. -DEFF_STDIO_PORT=4
 ninja Avionics-Argus3-SDR
 sudo /home/argus/effcc/bin/eff-flash apps/Avionics-Argus3-SDR/scalar/Avionics-Argus3-SDR.hex sram
 
-minicom -b 115200 -D /dev/ttyACM2
-
 cd apps/Avionics-Argus3-SDR
